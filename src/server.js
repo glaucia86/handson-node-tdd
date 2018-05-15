83ef6fe4de8fade640945942edb2f56b10221612
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-05-14 22:13:23 -0300

    Atualização arquivo 'server.js'

--- src/server.js
+++ src/server.js
@@ -7,18 +7,18 @@
  * 
  */
 
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var port = 8000;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const port = 8000;
 
-var post = require('./api/routes/post');
-var config = require('config'); // aqui estaremos carregando a localização da base de dados através dos arquivos JSON.
+const post = require('./api/routes/post');
+const config = require('config'); // aqui estaremos carregando a localização da base de dados através dos arquivos JSON.
 
 //Opção das base de dados:
-var options = {
+const options = {
     server: {
         socketOptions: {
             keepAlive: 1,
@@ -35,7 +35,7 @@ var options = {
 
 //Conexão com a base de dados:
 mongoose.connect(config.DBHost, options);
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erro ao conectar com a Base de Dados....: '));
 
 //Essa parte do código estaremos mostrando os logs quando acontecer os testes:
